3c5408687efcad9f59cd4149d755898468abb977
Joshua de Guzman <devjoshuadeguzman@gmail.com>
2018-04-19 10:03:41 +0800

    Update page titles

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('page-title')
+    | Create Task
+@endsection
+
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
